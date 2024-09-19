83260461996afa28ecd35d35c5f42b1550de9876
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-09-19 22:33:46 +0200

    refactor: Make temp file usage logic more robust

--- src/main/java/io/arconia/cli/utils/FileUtils.java
+++ src/main/java/io/arconia/cli/utils/FileUtils.java
@@ -12,10 +12,19 @@ import org.springframework.core.io.ClassPathResource;
 
 public final class FileUtils {
 
+    /// Get the absolute path of the current project directory.
+    ///
+    /// @return the absolute path of the current working directory
+    ///
     public static Path getProjectDir() {
         return Path.of(System.getProperty("user.dir")).toAbsolutePath();
     }
 
+    /// Find the given executable in the system's PATH.
+    ///
+    /// @param executableName   the name of the executable to find
+    /// @return                 the found executable file
+    ///
     public static File getExecutable(String executableName) {
         var pathEnv = System.getenv("PATH");
         if (pathEnv == null) {
@@ -33,6 +42,12 @@ public final class FileUtils {
         throw new RuntimeException("Cannot find the " + executableName + " executable in the PATH");
     }
 
+    /// Copy a file from the classpath to a temporary file.
+    ///
+    /// @param resourcePath     the path of the resource in the classpath
+    /// @return Path            the created temporary file
+    /// @throws IOException     if the file is not found or if there's an error during file operations
+    ///
     public static Path copyFileToTemp(String resourcePath) throws IOException {
         ClassPathResource resource = new ClassPathResource(resourcePath);
         
@@ -41,6 +56,7 @@ public final class FileUtils {
         }
 
         Path tempFile = Files.createTempFile("temp-", "-" + getFileNameFromPath(resourcePath));
+        tempFile.toFile().deleteOnExit();
         
         try (InputStream inputStream = resource.getInputStream()) {
             Files.copy(inputStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
