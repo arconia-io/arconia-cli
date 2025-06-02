6c5c9503e6b77d09d1671a1e46d1d51c32a90655
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-06-02 23:50:33 +0200

    refactor: Use more specific library for Gradle update recipe
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- src/main/java/io/arconia/cli/openrewrite/recipes/GradleRecipe.java
+++ src/main/java/io/arconia/cli/openrewrite/recipes/GradleRecipe.java
@@ -1,6 +1,6 @@
 package io.arconia.cli.openrewrite.recipes;
 
 public class GradleRecipe {
-    public static final String RECIPE_LIBRARY = "org.openrewrite:rewrite-java";
+    public static final String RECIPE_LIBRARY = "org.openrewrite:rewrite-gradle";
     public static final String RECIPE_NAME = "org.openrewrite.gradle.UpdateGradleWrapper";
 }
