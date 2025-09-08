58129bf2a6b737aa9f7b1e23f2a3748df8252307
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-09-09 01:15:03 +0200

    feat: Support Arconia 0.16.0

--- src/main/java/io/arconia/cli/commands/UpdateCommands.java
+++ src/main/java/io/arconia/cli/commands/UpdateCommands.java
@@ -67,7 +67,7 @@ public class UpdateCommands {
     public void updateArconia(
         CommandContext commandContext,
         @Option(description = "Update in dry-run mode.") boolean dryRun,
-        @Option(defaultValue = "0.15", description = "Arconia target version.") String toVersion,
+        @Option(defaultValue = "0.16", description = "Arconia target version.") String toVersion,
         @Option(description = "Include debug output.", shortNames = 'd') boolean debug,
         @Option(description = "Include more verbose output about the execution.", shortNames = 'v') boolean verbose,
         @Option(description = "Include more details about errors.", shortNames = 's') boolean stacktrace,
