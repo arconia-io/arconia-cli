bade8a4077f44c4a416d07702b306dfcd9b549d2
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-08-18 17:12:03 +0200

    build: Migrate from Flox to devenv
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ devenv.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.java = {
+    enable = true;
+    jdk.package = pkgs.graalvmPackages.graalvm-ce;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
