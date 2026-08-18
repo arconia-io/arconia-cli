15a348a6e634843896427ef04ff3ddb3fa5af948
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-08-18 17:30:16 +0200

    build: Adopt Nix in commit stage workflow
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,11 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.jreleaser-cli
+  ];
+
   # https://devenv.sh/languages/
   languages.java = {
     enable = true;
